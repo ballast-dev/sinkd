abd30864b438fa68d1b8bcc08228abbfc798c4fd
Tony B <krakjn@gmail.com>
2021-05-19 23:41:06 -0700

    Added yaml parser, might be better than serde_json

--- src/barge.rs
+++ src/barge.rs
@@ -9,16 +9,16 @@
  * be able to start and stop daemon 
  * this binary will be invoked in usr directory i.e. /usr/local/bin
  */
-
 extern crate notify;
+extern crate yaml_rust;
 
 use notify::{Watcher, RecursiveMode, watcher};
+use yaml_rust::{YamlLoader, YamlEmitter};
+
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::env;
 use std::path::PathBuf;
-
-
 
 pub struct AnchorPoint {
     // directory to watch
@@ -53,6 +53,53 @@ fn watch(dir_to_watch: &str, interval: u32) -> bool {
 }
 
 
+// pub fn load_arc(arc: &mut Arc) {
+//     /*
+//      * the location of arc.json will determine the root directory of arc
+//      */
+//     if !find_arc(arc) {
+//         println!("couldn't find 'arc.json' in this directory or any parent directory");
+//         exit(1);
+//     }
+
+//     let arcj: ArcJson;
+//     let contents: String;
+
+//     let read_result = fs::read_to_string(&arc.path.arc);
+//     match read_result {
+//         Err(_) => {
+//             println!("Couldn't read 'arc.json'");
+//             exit(1);
+//         },
+//         Ok(c) => contents = c,
+//     }
+
+//     let result = serde_json::from_str(&contents[..]);
+//     match result {
+//         Err(_) => {
+//             println!("couldn't parse arc.json, is it valid json?");
+//             exit(1);
+//         }
+//         Ok(json) => arcj = json,
+//     }
+
+//     arc.remote.name = arcj.remote.name;
+//     arc.remote.url  = arcj.remote.url;
+
+//     for (k, v) in arcj.projects {
+//         // println!("..... --> {}", k);
+//         let mut r = Repo{name: k, deps: v};
+//         // println!("loaded arc repo....{}", &r.name);
+//         arc.repos.push(r);
+//     }
+// }
+
+fn load_config() -> bool {
+    // config should always be located in /etc/sinkd.conf
+    // fs::read
+    return true;
+}
+
 /**
  * upon edit of configuration restart the daemon
  */
@@ -61,6 +108,39 @@ pub fn start_daemon() {
     // need to start daemon from config file
     // need to parse json
     // set up json within /etc/sinkd.json
+
+    let s =
+"
+foo:
+    - list1
+    - list2
+bar:
+    - 1
+    - 2.0
+";
+    let docs = YamlLoader::load_from_str(s).unwrap();
+
+    // Multi document support, doc is a yaml::Yaml
+    let doc = &docs[0];
+
+    // Debug support
+    println!("{:?}", doc);
+
+    // Index access for map & array
+    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
+
+    // Chained key/array access is checked and won't panic,
+    // return BadValue if they are not exist.
+    assert!(doc["INVALID_KEY"][100].is_badvalue());
+
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+    }
+    println!("{}", out_str);
 
     // serde already does json
 
