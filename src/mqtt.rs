332b3b0a233cfabb8616f09a0a4f3de427555499
Tony B <krakjn@gmail.com>
2021-05-19 23:44:50 -0700

    Asynchronous Client with callback to kill proc

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,65 +1,96 @@
 use std::process;
 use paho_mqtt as mqtt;
 
+
+// Callback for a successful connection to the broker.
+// We subscribe to the topic(s) we want here.
+fn on_connect_success(cli: &mqtt::AsyncClient, _msgid: u16) {
+    println!("Connection succeeded");
+    cli.subscribe("sinkd/#", mqtt::QOS_0);
+}
+
+// Callback for a failed attempt to connect to the server.
+// We simply sleep and then try again.
+//
+// Note that normally we don't want to do a blocking operation or sleep
+// from  within a callback. But in this case, we know that the client is
+// *not* conected, and thus not doing anything important. So we don't worry
+// too much about stopping its callback thread.
+fn on_connect_failure(cli: &mqtt::AsyncClient, _msgid: u16, rc: i32) {
+    println!("Connection attempt failed with error code {}.\n", rc);
+    std::thread::sleep(std::time::Duration::from_millis(2500));
+    cli.reconnect_with_callbacks(on_connect_success, on_connect_failure);
+}
+
+
+
+
+fn callme() {
+    println!("callback invoked!");
+}
+
 pub fn listen() {
+    let (tx, rx) = std::sync::mpsc::channel();
+
     // Create a client & define connect options
-    let cli = mqtt::Client::new("tcp://localhost:1883").unwrap_or_else(|err| {
+    let mut cli = mqtt::AsyncClient::new("tcp://localhost:1883").unwrap_or_else(|err| {
         println!("Error creating the client: {:?}", err);
         process::exit(1);
     });
 
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(std::time::Duration::from_secs(20))
-        .clean_session(true)
-        .finalize();
-
-    // Connect and wait for it to complete or fail
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect:\n\t{:?}", e);
-        process::exit(1);
-    }
-
     // Create a message and publish it
     let msg = mqtt::Message::new("sinkd/", "sinkd is gonna kick ass", 0);
 
-    if let Err(e) = cli.publish(msg) {
-        println!("Error sending message: {:?}", e);
-    }
+    cli.publish(msg);
 
     let disconn_opts = mqtt::DisconnectOptionsBuilder::new()
                     .reason_code(mqtt::ReasonCode::default())
                     .finalize();
 
-    // // Attach a closure to the client to receive callback
-    // // on incoming messages.
-    // cli.set_message_callback(|cli,msg| {
-    //     if let Some(msg) = msg {
-    //         let topic = msg.topic();
-    //         let payload_str = msg.payload_str();
+    // Attach a closure to the client to receive callback
+    // on incoming messages.
+    cli.set_message_callback(move |_cli,msg| {
+        if let Some(msg) = msg {
+            let topic = msg.topic();
+            let payload_str = msg.payload_str();
 
-    //         if topic == "requests/subscription/add" {
-    //             let data = cli.user_data().unwrap();
-    //             if let Some(lock) = data.downcast_ref::<UserTopics>() {
-    //                 let mut topics = lock.write().unwrap();
-    //                 let new_topic = payload_str.to_owned().to_string();
-    //                 println!("Adding topic: {}", new_topic);
-    //                 cli.subscribe(&new_topic, QOS);
-    //                 topics.push(new_topic);
-    //             }
-    //             else {
-    //                 println!("Failed to add topic: {}", payload_str);
-    //             }
-    //         }
-    //         else {
-    //             println!("{} - {}", topic, payload_str);
-    //         }
-    //     }
-    // });
-
-    // Disconnect from the broker
-    if let Err(e) = cli.disconnect(disconn_opts) {
-        println!("Error disconnecting, {:?}", e);
-        process::exit(1);
+            if topic == "sinkd/general" {
+                println!("{} => {}", topic, payload_str);
+            } else if topic == "sinkd/offtopic" {
+                callme();
+            } else if topic == "sinkd/discon" {
+                // Disconnect from the broker
+                println!("setting running to false");
+                if let Err(e) = tx.send(false) {
+                    println!("couldn't send! {:?}", e);
+                }
+            } else {
+                println!("{} - {}", topic, payload_str);
+            }
+        }
+    });
+    
+    let lwt = mqtt::Message::new("sinkd/lost_conn", "Async subscriber lost connection", 1);
+    
+    let conn_opts = mqtt::ConnectOptionsBuilder::new()
+    .keep_alive_interval(std::time::Duration::from_secs(20))
+    .mqtt_version(mqtt::MQTT_VERSION_3_1_1)
+    .clean_session(true)
+    .will_message(lwt)
+    .finalize();
+    
+    // Make the connection to the broker
+    println!("Connecting to the MQTT server...");
+    cli.connect_with_callbacks(conn_opts, on_connect_success, on_connect_failure);
+    
+    println!("Waiting for messages...");
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        if let Ok(val) = rx.try_recv() {
+            println!("got it! {:?}", val);
+            cli.disconnect(disconn_opts.clone());
+            std::process::exit(0);
+        }
     }
-    // tok.wait().unwrap();
+    
 }
